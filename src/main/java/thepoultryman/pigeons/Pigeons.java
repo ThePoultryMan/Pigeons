3ffa80913cc9438c3c286b5c290c594e6f1be1c5
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-01-31 17:57:05 -0500

    Initial commit

--- src/main/java/thepoultryman/pigeons/Pigeons.java
+++ src/main/java/thepoultryman/pigeons/Pigeons.java
@@ -1,15 +1,15 @@
-package thepoultryman.modid;
+package thepoultryman.pigeons;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModClass implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class Pigeons implements ModInitializer {
+	public static final String MOD_ID = "pigeons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		LOGGER.info("Initializing the attack of the pigeons");
 	}
 }
