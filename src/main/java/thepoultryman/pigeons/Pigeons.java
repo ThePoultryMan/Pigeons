48a20da11feb69aff86d6d5705daa6f4283d95a6
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-01-31 17:59:51 -0500

    Re-indent

--- src/main/java/thepoultryman/pigeons/Pigeons.java
+++ src/main/java/thepoultryman/pigeons/Pigeons.java
@@ -5,11 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Pigeons implements ModInitializer {
-	public static final String MOD_ID = "pigeons";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "pigeons";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Initializing the attack of the pigeons");
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing the attack of the pigeons");
+    }
 }
