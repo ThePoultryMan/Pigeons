5d64b88af55e3f867d91cd39de8865b503585a23
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-02-06 17:29:05 -0500

    Add comments

--- src/main/java/thepoultryman/registry/ItemRegistry.java
+++ src/main/java/thepoultryman/registry/ItemRegistry.java
@@ -7,10 +7,12 @@ import net.minecraft.util.registry.Registry;
 import thepoultryman.pigeons.Pigeons;
 
 public class ItemRegistry {
+    // Accessories
     public static final Item TOP_HAT = new Item(new Item.Settings().group(ItemGroup.TOOLS));
     public static final Item BEANIE = new Item(new Item.Settings().group(ItemGroup.TOOLS));
 
     public static void registerItems() {
+        // Accessories
         register("top_hat", TOP_HAT);
         register("beanie", BEANIE);
     }
