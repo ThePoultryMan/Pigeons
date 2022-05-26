5cd8c6d10629cecaf0e97d5b1e66a972543506ed
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-25 23:56:07 -0400

    Redo info and button enabling logic

--- src/main/java/thepoultryman/pigeons/screen/LetterGui.java
+++ src/main/java/thepoultryman/pigeons/screen/LetterGui.java
@@ -4,13 +4,16 @@ import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.*;
 import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
 public class LetterGui extends LightweightGuiDescription {
     private static final Text ENTER_COORDS_TEXT = new TranslatableText("gui.pigeons.info.enterCoords");
-    private static final Text[] INVALID_COORD_TEXT = new Text[] {new TranslatableText("gui.pigeons.info.coordX"),
-            new TranslatableText("gui.pigeons.info.coordY"), new TranslatableText("gui.pigeons.info.coordZ")};
+    private static final Text[] INVALID_COORD_TEXT = new Text[] {new TranslatableText("gui.pigeons.info.invalid.coordX"),
+            new TranslatableText("gui.pigeons.info.invalid.coordY"), new TranslatableText("gui.pigeons.info.invalid.coordZ")};
+    private static final Text[] MISSING_COORD_TEXT = new Text[] {new TranslatableText("gui.pigeons.info.missing.coordX"),
+            new TranslatableText("gui.pigeons.info.missing.coordY"), new TranslatableText("gui.pigeons.info.missing.coordZ")};
 
     public LetterGui() {
         WGridPanel root = new WGridPanel();
@@ -29,36 +32,53 @@ public class LetterGui extends LightweightGuiDescription {
         infoText.setHorizontalAlignment(HorizontalAlignment.CENTER);
         root.add(infoText, 1, 5, 8, 1);
 
-        WTextField xCoord = new WTextField(new TranslatableText("gui.pigeons.xCoord"));
-        xCoord.setChangedListener((s -> {
-            infoText.setText(changeInfoText(xCoord.getText(), 0));
-        }));
-        root.add(xCoord, 1, 3, 2, 1);
-        WTextField yCoord = new WTextField(new TranslatableText("gui.pigeons.yCoord"));
-        yCoord.setChangedListener((s -> {
-            infoText.setText(changeInfoText(yCoord.getText(), 1));
-        }));
-        root.add(yCoord, 4, 3, 2, 1);
-        WTextField zCoord = new WTextField(new TranslatableText("gui.pigeons.zCoord"));
-        zCoord.setChangedListener((s -> {
-            infoText.setText(changeInfoText(zCoord.getText(), 2));
-        }));
-        root.add(zCoord, 7, 3, 2, 1);
+        WTextField[] coordinateFields = new WTextField[] {new WTextField(new TranslatableText("gui.pigeons.xCoord")),
+                new WTextField(new TranslatableText("gui.pigeons.yCoord")), new WTextField(new TranslatableText("gui.pigeons.zCoord"))};
+        root.add(coordinateFields[0], 1, 3, 2, 1);
+        root.add(coordinateFields[1], 4, 3, 2, 1);
+        root.add(coordinateFields[2], 7, 3, 2, 1);
 
-        WButton button = new WButton(new TranslatableText("gui.pigeons.sealLetter"));
-        root.add(button, 3, 7, 4, 1);
+        WButton sealButton = new WButton(new TranslatableText("gui.pigeons.sealLetter"));
+        root.add(sealButton, 3, 7, 4, 1);
+
+        coordinateFields[0].setChangedListener((s -> {
+            updateInfoAndButton(coordinateFields, infoText, sealButton);
+        }));
+        coordinateFields[1].setChangedListener((s -> {
+            updateInfoAndButton(coordinateFields, infoText, sealButton);
+
+        }));
+        coordinateFields[2].setChangedListener((s -> {
+            updateInfoAndButton(coordinateFields, infoText, sealButton);
+        }));
 
         root.validate(this);
     }
 
-    private static Text changeInfoText(String fieldText, int coordInt) {
-        try {
-            Integer coord = Integer.valueOf(fieldText);
-        } catch (NumberFormatException ignored) {
-            if (!fieldText.equals(""))
-                return INVALID_COORD_TEXT[coordInt];
+    private static void updateInfoAndButton(WTextField[] coordinateFields, WText infoText, WButton sealButton) {
+        for (int i = 0; i < coordinateFields.length; ++i) {
+            String fieldText = coordinateFields[i].getText();
+            Integer coordinate = tryIntegerParse(fieldText);
+            if (coordinate == null && !fieldText.equals("")) {
+                infoText.setText(INVALID_COORD_TEXT[i]);
+                sealButton.setEnabled(false);
+                break;
+            } else if (fieldText.equals("")) {
+                infoText.setText(MISSING_COORD_TEXT[i]);
+                sealButton.setEnabled(false);
+                break;
+            } else {
+                infoText.setText(new LiteralText(""));
+                sealButton.setEnabled(true);
+            }
         }
+    }
 
-        return new TranslatableText("");
+    private static Integer tryIntegerParse(String string) {
+        try {
+            return Integer.valueOf(string);
+        } catch (NumberFormatException ignored) {
+            return null;
+        }
     }
 }
