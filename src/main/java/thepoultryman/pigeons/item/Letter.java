e1f60f0d16d08f6308fd6d5568d7046cb2e9ad4f
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-26 15:00:51 -0400

    Make unable to put items in sealed letters

--- src/main/java/thepoultryman/pigeons/item/Letter.java
+++ src/main/java/thepoultryman/pigeons/item/Letter.java
@@ -3,11 +3,14 @@ package thepoultryman.pigeons.item;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
 import net.minecraft.item.BundleItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ClickType;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -20,6 +23,22 @@ import java.util.List;
 public class Letter extends BundleItem {
     public Letter(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public boolean onStackClicked(ItemStack stack, Slot slot, ClickType clickType, PlayerEntity player) {
+        if (!isSealed(stack))
+            return super.onStackClicked(stack, slot, clickType, player);
+        else
+            return false;
+    }
+
+    @Override
+    public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+        if (!isSealed(stack))
+            return super.onClicked(stack, otherStack, slot, clickType, player, cursorStackReference);
+        else
+            return false;
     }
 
     @Override
