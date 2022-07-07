2fad8a5d36771f1e046adede31c6c5bf6b7d5c71
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-06 21:33:48 -0400

    Add the other pigeon types

--- src/main/java/thepoultryman/pigeons/config/PigeonsConfig.java
+++ src/main/java/thepoultryman/pigeons/config/PigeonsConfig.java
@@ -11,6 +11,9 @@ public class PigeonsConfig {
     private final FileConfig config;
 
     private ItemStack cityDrop;
+    private ItemStack antwerpBrownDrop;
+    private ItemStack antwerpGrayDrop;
+    private ItemStack egyptianDrop;
 
     public PigeonsConfig() {
         this.config = FileConfig.builder(FabricLoader.getInstance().getConfigDir() + "/pleasant-pigeons.toml").defaultResource("/pleasant-pigeons.toml").autosave().build();
@@ -19,7 +22,14 @@ public class PigeonsConfig {
     public void loadConfig() {
         this.config.load();
 
-        this.cityDrop = this.getSpecialDrop(this.config.get("special_drops.city.item"), this.config.get("special_drops.city.count"));
+        this.cityDrop = this.getSpecialDrop(this.config.getOrElse("special_drops.city.item", "minecraft:diamond"),
+                this.config.getOrElse("special_drops.city.count", 1));
+        this.antwerpBrownDrop = this.getSpecialDrop(this.config.getOrElse("special_drops.antwerp_brown.item", "minecraft:raw_iron"),
+                this.config.getOrElse("special_drops.antwerp_brown.count", 3));
+        this.antwerpGrayDrop = this.getSpecialDrop(this.config.getOrElse("special_drops.antwerp_gray.item", "minecraft:raw_copper"),
+                this.config.getOrElse("special_drops.antwerp_gray.count", 7));
+        this.egyptianDrop = this.getSpecialDrop(this.config.getOrElse("special_drops.egyptian.item", "minecraft:cooked_beef"),
+                this.config.getOrElse("special_drops.egyptian.count", 5));
     }
 
     private ItemStack getSpecialDrop(String itemIdentifier, int count) {
